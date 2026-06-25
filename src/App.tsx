d5dcd475a62a17f9a9ad14c0d502ad0db3006a5b
Roroz7 <pepettteee@gmail.com>
2026-06-25 15:28:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import Header from './Header'
 import './App.css'
 import Navbar from './Navbar'
 import Contents from './Contents'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div id='entete'>
